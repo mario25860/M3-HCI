8e10fadace289fd07acbf5ea84d2aa172522b28c
mario25860 <angeleskimario95@gmail.com>
2024-05-14 17:53:41 +0200

    Test2

--- KitchenKarma/App.js
+++ KitchenKarma/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Here are Changes!</Text>
+      <Text>Here are Chanes!</Text>
       <StatusBar style="auto" />
     </View>
   );
