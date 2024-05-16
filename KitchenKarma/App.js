cd7d893fc5f5038b39b4319b27816e810f40acc2
mario25860 <angeleskimario95@gmail.com>
2024-05-16 13:14:27 +0200

    screens hinzugefügt

--- KitchenKarma/App.js
+++ KitchenKarma/App.js
@@ -1,6 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+/*
+Startscreen:
+Profil ausfüllen und Präferenzen
+
+-> Overview:
+MyFridge -> neuer Screen der Zutaten - wählbar und Rezept generieren - hinzufügen der Lebensmittel mit Suchpunktion
+
+Gruppe -> Gruppen anlegen mit Präferenzen und wählbar
+
+-> Make Meal:
+Rezepte generieren ohne bestimmte Lebensmittel
+
+-> Profil:
+Profil anpassen wie am Anfang
+*/
+
 export default function App() {
   return (
     <View style={styles.container}>
